5f0671d4895d7a7c55cb8be3cea066a1c88de4fe
Gopala <guptaraja727@gmail.com>
2022-01-01 23:09:40 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require("express")
 
+const mongoose = require("mongoose")
+
+//------connecting----------
+const connect = (()=>{
+    return mongoose.connect("mongodb://127.0.0.1:27017/Gopala")
+}) 
+
+//---------schema------
+const userSche = new mongoose.Schema({
+    movies_name:{type:String,required:true},
+    movie_genre:{type:String,required:true},
+    production_year:{type:Number,required:true},
+    budget:{type:Number,required:true}
+})
+//-------schema-model-------
+const User = mongoose.model("user",userSche)
+
 const app = express()
 
+app.get("/",async (req,res)=>{
+    const user = await User.find().lean().exec()
+    return res.send(user)
+})
 
-app.listen(2435,(req,res)=>{
-console.log("Listening from 2435")
+app.listen(2345,async (req,res)=>{
+    await connect()
+console.log("Listening from 2345")
 })
